75fbd76eda7ecca44f40ce11ec0c4eee379c24ed
Alexander Talbott <atalbott13@gmail.com>
2020-06-19 14:09:31 -0400

    set up list page with dummy data (siteMetadata)

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,54 @@ module.exports = {
       name: `tictic dev team`,
     },
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    testListData: {
+      title: "Game of Thrones",
+      description: "What's west of Westeros? We don't know, but here are some known hangouts for GoT characters.",
+      image: {
+        imageId: "list-cover-images/category-game-of-thrones_nb6s4e",
+        baseUrl: "https://res.cloudinary.com/ticticapp/image/upload/",
+      },
+      items: [
+      {
+        itemId: "2a0a86f9-b3df-4fe7-94a3-79616674ec75",
+        name: "Alcázar of Seville",
+        images: [
+          {
+            imageId: "places-images-new/338337_Viator_Shutterstock_171366_bvf25n",
+            baseUrl: "https://res.cloudinary.com/ticticapp/image/upload/"
+          }
+        ],
+      },
+      {
+        itemId: "d4c6ecd1-2d1d-4de0-8e94-f24715562b7e",
+        name: "Dark Hedges",
+        images: [
+          {
+            imageId: "places-images-new/Dark-Hedges",
+            baseUrl: "https://res.cloudinary.com/ticticapp/image/upload/"
+          }
+        ]
+      },
+      {
+        itemId: "e1a3a26c-3975-42b9-9872-1f88b15bd71d",
+        name: "Walls of Dubrovnik",
+        images: [
+          {
+            imageId: "places-images-new/5fa4f698-47f3-40e1-abb1-9809a9859591",
+            baseUrl: "https://res.cloudinary.com/ticticapp/image/upload/"
+          },
+          {
+            imageId: "places-images-new/eaf1c3e3-c5b6-4aba-993e-53dd39920d99",
+            baseUrl: "https://res.cloudinary.com/ticticapp/image/upload/"
+          },
+          {
+            imageId: "places-images-new/95e9ac7d-ba53-4ef1-abc1-9dbb61fc0fbc",
+            baseUrl: "https://res.cloudinary.com/ticticapp/image/upload/"
+          },
+        ]
+      },
+      ],
+    },
   },
   plugins: [
     {
---
+++ src/pages/list.js
@@ -0,0 +1,103 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm, scale } from "../utils/typography"
+
+const ListPage = ({ data, location }) => {
+  {/*const list = data.getList*/}
+  {/* 
+      THIS IS TEST DATA 
+      REMEMBER TO DELETE LATER
+      IN SITEMETADATA gatsby-config.js 
+
+      TODO -- MOVE THIS PAGE TO A TEMPLATE
+      TODO -- SET UP SLUGS
+  */}
+  const siteTitle = data.site.siteMetadata.title
+  const list = data.site.siteMetadata.testListData
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO
+        title={list.title}
+        description={list.description}
+      />
+      <article>
+        <header>
+          <img src={list.image.baseUrl + list.image.imageId} alt={list.title} />
+          <h1
+            style={{
+              marginTop: rhythm(1),
+              marginBottom: 0,
+            }}
+          >
+            {list.title}
+          </h1>
+        </header>
+        <hr
+          style={{
+            marginBottom: rhythm(1),
+          }}
+        />
+      </article>
+      {list.items.map(({ itemId, name, images }) => (
+        <div key={itemId}>
+          <img src={images[0].baseUrl + images[0].imageId} alt="" />
+          <h3>{name}</h3>
+        </div>
+      ))}
+
+    </Layout>
+  )
+}
+
+export default ListPage
+
+export const listQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        testListData {
+          title
+          description
+          image {
+            imageId
+            baseUrl
+          }
+          items {
+            itemId
+            name
+            images {
+              imageId
+              baseUrl
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+{/*
+  query {
+    getList($slug: String!) {
+      title
+      description
+      image {
+        imageId
+        baseUrl
+      }
+      items{
+        itemId
+        name
+        images {
+          imageId
+          baseUrl
+        }
+      }
+    }
+  }
+`
+*/}
