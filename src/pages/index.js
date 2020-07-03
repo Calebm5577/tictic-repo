a0815cd1be56beea25d08a5932262342293c03ba
Alexander Talbott <atalbott13@gmail.com>
2020-07-03 14:07:27 -0400

    working on client side env variable

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const PageIndex = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
-      <div>{process.env.API_SERVER_URL}</div>
       <Link to = "/item/16df4e41-76c5-480e-b7ad-f828e82aad93">Sample Item</Link><br></br>
       <Link to = "/list/3f204bad-f0d5-48c5-938b-05c39822a9c3">Sample List</Link>
     </Layout>
